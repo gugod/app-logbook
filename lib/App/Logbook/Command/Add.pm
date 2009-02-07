d0710f7d636e8013a540076a9baecae4e39b3605
Liu Kang-min <gugod@gugod.org>
2009-02-08 02:57:12 +0800

    let format be an command line option.

--- lib/App/Logbook/Command/Add.pm
+++ lib/App/Logbook/Command/Add.pm
@@ -6,15 +6,24 @@ use feature ':5.10';
 use base 'App::CLI::Command';
 
 use App::Logbook::DB;
-use App::Logbook::Text;
+use UNIVERSAL::require;
 
 use self;
+
+my $format = "text";
+
+sub options {(
+    'format=s' => \$format,
+)}
 
 sub run {
     my $dir = App::Logbook::DB->connect;
     my $scope = $dir->new_scope;
 
-    my $obj = App::Logbook::Text->new( content => join(" ", @args) );
+    my $entry_class = "App::Logbook::" . ucfirst( lc($format) );
+    $entry_class->require or die "Unkonw logbook format: $format\n";
+
+    my $obj = $entry_class->new( content => join(" ", @args) );
     $dir->store($obj);
 
     say "... stored";
