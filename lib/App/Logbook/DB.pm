57b25b95542d77b031faaff14d76725f4c558de7
Kang-min Liu <gugod@gugod.org>
2014-10-13 20:03:07 +0200

    use File::XDG for its data_home  convention.

--- lib/App/Logbook/DB.pm
+++ lib/App/Logbook/DB.pm
@@ -3,22 +3,18 @@ use strict;
 use warnings;
 
 use KiokuDB;
+use File::XDG;
 use File::Path qw(make_path);
+use File::Spec;
 
 sub connect {
-    unless (defined $ENV{HOME}) {
-	die "logbook can't work without \$HOME.";
-    }
+    my $xdg = File::XDG->new( name => "logbook" );
 
-    unless (-d "$ENV{HOME}/.logbook/") {
-	warn "Will create dir: $ENV{HOME}/.logbook/\n";
-	make_path("$ENV{HOME}/.logbook/");
-    }
+    my $data_home = $xdg->data_home;
+    make_path($data_home, { verbose => 1, mode => 0700 }) unless -d $data_home;
 
-    return KiokuDB->connect(
-	"dbi:SQLite:dbname=$ENV{HOME}/.logbook/logbook.db",
-	create => 1
-    );
+    my $sqlite_path = File::Spec->catfile($data_home, "logbook.db");
+    return KiokuDB->connect("dbi:SQLite:dbname=${sqlite_path}", create => 1);
 }
 
 1;
